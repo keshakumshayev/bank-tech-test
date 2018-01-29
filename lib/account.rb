d2abd78306d053a0c434fa403e51ef58e948ddb3
kkumshayev <kumshkesh@gmail.com>
2018-01-29 12:29:03 +0000

    failing test for deposit

--- lib/account.rb
+++ lib/account.rb
@@ -1,4 +1,9 @@
 class Account
+  attr_reader :balance
+  def initialize()
+    @balance = 0
+  end
+
   def deposit(amount)
 
   end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,16 @@
 require 'account'
 
 describe Account do
+  TEST_DEPOSIT = 20
+  TEST_WITHDRAWAL = 10
+
   it { is_expected.to respond_to(:deposit).with(1).argument }
   it { is_expected.to respond_to(:withdraw).with(1).argument }
+
+  it 'can be deposited into' do
+    test_account = Account.new()
+
+    expect{test_account.deposit(TEST_DEPOSIT)}.to change{test_account.balance}.by(TEST_DEPOSIT)
+  end
+
 end
